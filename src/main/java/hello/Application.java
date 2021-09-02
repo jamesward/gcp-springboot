25aa0058313cafdcc58eb972ae5050babe5936b2
aaronmwanjala <aaronmwanjala@google.com>
2021-09-02 16:06:20 -0400

    feat: structured logging endpoints and config

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,5 +1,7 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+  Logger logger = LoggerFactory.getLogger(Application.class);
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
   @RequestMapping("/")
-  public String index() {
-    return "hello, world";
+  public void info() {
+    logger.info("Endpoint was hit at " + System.currentTimeMillis());
+  }
+
+  @RequestMapping("/error")
+  public void error() {
+    logger.error("Error was logged at " + System.currentTimeMillis());
   }
 
 }
