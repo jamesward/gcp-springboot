fc8243128d2988079fbff3ddda8b1858e300bc9a
James Ward <james@jamesward.com>
2021-09-04 08:51:19 -0600

    trace

--- README.md
+++ README.md
@@ -3,18 +3,15 @@ GCP Spring Boot
 
 [![Run on Google Cloud](https://deploy.cloud.run/button.svg)](https://deploy.cloud.run)
 
-# todo: service account stuff
-
 Or Command Line with Cloud Run source deploy (Google Cloud Buildpacks):
 ```
 export PROJECT_ID=YOUR_PROJECT_ID
 
-gcloud alpha run deploy gcp-springboot-metrics \
+gcloud run deploy gcp-springboot-trace \
   --platform=managed \
   --region=us-central1 \
   --project=$PROJECT_ID \
   --allow-unauthenticated \
-  --no-cpu-throttling \
   --source=.
 ```
 
@@ -23,15 +20,14 @@ Or Command Line with Spring's Buildpacks:
 export PROJECT_ID=YOUR_PROJECT_ID
 
 ./mvnw spring-boot:build-image \
-  -Dspring-boot.build-image.imageName=gcr.io/$PROJECT_ID/gcp-springboot:metrics
+  -Dspring-boot.build-image.imageName=gcr.io/$PROJECT_ID/gcp-springboot:trace
 
-docker push gcr.io/$PROJECT_ID/gcp-springboot:metrics
+docker push gcr.io/$PROJECT_ID/gcp-springboot:trace
 
-gcloud alpha run deploy gcp-springboot-metrics \
+gcloud run deploy gcp-springboot-trace \
   --platform=managed \
   --region=us-central1 \
   --project=$PROJECT_ID \
   --allow-unauthenticated \
-  --no-cpu-throttling \
-  --image=gcr.io/$PROJECT_ID/gcp-springboot:metrics
+  --image=gcr.io/$PROJECT_ID/gcp-springboot:trace
 ```
--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,20 +1,40 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @RestController
 public class Application {
+
+  @Bean
+  WebClient webClient() {
+    return WebClient.create();
+  }
+
+  @Autowired WebClient webClient;
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
   @RequestMapping("/")
-  public String index() {
+  public Mono<String> index(@RequestHeader(name="Host") final String host) {
+    // todo: protocol match
+    String url = "https://" + host + "/hello";
+    return webClient.get().uri(url).retrieve().bodyToMono(String.class).map(String::toUpperCase);
+  }
+
+  @GetMapping("/hello")
+  public String hello() {
     return "hello, world";
   }
 
