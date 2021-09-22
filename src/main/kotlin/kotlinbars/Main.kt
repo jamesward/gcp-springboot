525315c73ef634a2504b3bbbdaac80f33b474fdd
James Ward <james@jamesward.com>
2021-09-22 14:11:37 -0600

    tracing

--- src/main/kotlin/kotlinbars/Main.kt
+++ src/main/kotlin/kotlinbars/Main.kt
@@ -2,20 +2,40 @@ package kotlinbars
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
 
 
 @SpringBootApplication
 @RestController
-class WebApp {
+class WebApp(val webClient: WebClient) {
 
     @GetMapping("/")
-    fun index(): String {
-        return "hello, world"
+    suspend fun index(@RequestHeader(name="Host") host: String): String {
+        // todo: protocol match
+        val url = "http://" + host + "/hello";
+        val response = webClient.get().uri(url).retrieve().awaitBody<String>()
+        return response.uppercase()
     }
 
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "hello, world";
+    }
+
+    companion object {
+        @Bean
+        fun webClient(): WebClient {
+            return WebClient.create();
+        }
+    }
 }
+
+
 
 fun main(args: Array<String>) {
     runApplication<WebApp>(*args)
